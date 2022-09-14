8d5e606eaa57a0150dcab36cbcdcc899c989a52b
AlmasObinna <almasobinna@gmail.com>
2022-09-13 21:55:34 -0500

    provider change

--- Provider.tf
+++ Provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA2UJXDMJPJ27NAUPU"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
